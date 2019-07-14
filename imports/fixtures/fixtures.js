f2c44f568032424b0fc0f03fcb283dc92260cccb
James Raubenheimer <raubenheimerjames@gmail.com>
2019-07-14 17:28:07 -0400

    Integrate selectedNoteId into list items and url

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,17 @@
+
+
+export const notes = [
+    {
+        _id: 'noteId1',
+        title: 'Test title',
+        body: '',
+        updatedAt: 1563134096143,
+        userId: 'userId1',
+    }, {
+        _id: 'noteId2',
+        title: '',
+        body: 'Something is here',
+        updatedAt: 1563134096143,
+        userId: 'userId2',
+    }
+];
